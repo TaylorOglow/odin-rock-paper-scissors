8165cb8b1f81ff1bf1258ad684473d85745c9d52
Taylor Oglow <98560933+TaylorOglow@users.noreply.github.com>
2022-06-27 16:06:38 -0700

    Added winner decleration dialogue

--- external.js
+++ external.js
@@ -1,10 +1,85 @@
 
-function computerPlay() {
-    const rpsArray = ["Rock", "Paper", "Scissors"];
-    const rpsRandom = rpsArray[Math.floor(Math.random() * rpsArray.length)];
-    console.log(rpsRandom);
+// Computer selection function
+
+function computerChoice() {
+    const rpsArray = ["rock", "paper", "scissors"];
+    const computerSelection = rpsArray[Math.floor(Math.random() * rpsArray.length)];
+    console.log(computerSelection());
 }
-computerPlay(); 
+
+// Player selection function 
+
+function playerChoice() {
+    const playerChoice = prompt('Please type "rock, paper, or scissors').toLocaleLowerCase();
+}
+
+// Playing a single round 
+
+function singleRound(playerChoice, computerChoice) {
+    computerPlay();
+} 
+
+// Winner Declaration
+
+function winnerDeclaration (playerChoice, computerChoice) {
+    if (playerChoice === "rock" && computerChoice === "scissors") {
+        console.log("You selected rock. Captain Goldfinch selected scissors. You WON!")
+    }
+        (playerChoice === "rock" && computerChoice === "paper") {
+            console.log("You selected rock. Captain Goldfinch selected paper. Captain Goldfinch WON!")
+    }
+
+    (playerChoice === "rock" && computerChoice === "rock") {
+        console.log("You selected rock. Captain Goldfinch selected rock. It's a tie!")
+    }
+    (playerChoice === "paper" && computerChoice === "rock") {
+        console.log("You selected paper. Captain Goldfinch selected rock. You WON!")
+    }
+    (playerChoice === "paper" && computerChoice === "scissors") {
+    console.log("You selected paper. Captain Goldfinch selected scissors. Captain Goldfinch WON!")
+    }
+(playerChoice === "paper" && computerChoice === "paper") {
+    console.log("You selected paper. Captain Goldfinch selected paper. It's a tie!")
+    }
+    (playerChoice === "scissors" && computerChoice === "rock") {
+    console.log("You selected scissors. Captain Goldfinch selected rock. Captain Goldfinch WON!")
+    }
+    (playerChoice === "scissors" && computerChoice === "paper") {
+    console.log("You selected scissors. Captain Goldfinch selected paper. You WON!")
+    }
+(playerChoice === "scissors" && computerChoice === "scissors") {
+    console.log("You selected scissors. Captain Goldfinch selected scissors. It's a tie!")
+    }
+
+}
+
+
+console.log(singleRound());
+
+
+
+
+    // const rpsArray = ["Rock", "Paper", "Scissors"];
+    // const playerCue = "Please select an object";
+    // const playerSelection = "rock"; 
+    // const computerSelection = computerPlay();
+    // console.log(singleRound(playerSelection, computerSelection));
+
+
+    // if computerSelection === "Rock"
+    // else if
+    // if computerSelection === "Paper"
+    // if computerSelection === "Scissors"
+
+    // if playerSelection === "Rock"
+    // if playerSelection === "Paper"
+    // if playerSelection === "Scissors"
+
+
+
+
+
+
 
 // Initial Screen: Input name. 
 
@@ -14,6 +89,8 @@ computerPlay();
 
 // "The WINNER gets gold"
 // "The LOSER walks the plank"
+
+
 
 // "Beat Captain Goldfinch to win his pot of gold!"
 
